6276145d88ec394caa17153b64a2b7f5313a7502
韩文博 <9476400@qq.com>
2018-10-07 22:15:39 +0800

    no message

---
+++ App/HttpController/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\HttpController;
+
+use App\Utils\Code;
+
+class Index
+{
+	/**
+	 * 买家设置默认收货地址
+	 * @method POST
+	 * @param int $id 地址id
+	 * @author 韩文博
+	 */
+	public function index()
+	{
+
+//			$this->send( Code::success );
+	}
+}
