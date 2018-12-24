d62bc57721dc054c64324b696c05cbca996e8ad8
孙泉 <582194766@qq.com>
2018-12-24 11:46:47 +0800

    新增了后台修改订单价格

--- App/Validate/Admin/Order.php
+++ App/Validate/Admin/Order.php
@@ -14,52 +14,99 @@
 namespace App\Validate\Admin;
 
 use ezswoole\Validate;
-use ezswoole\Db;
-use App\Logic\VerifyCode;
 
 class Order extends Validate
 {
-	protected $rule
-		= [
-			'id'              => 'require',
-			'shipper_id'      => 'require',
-			'express_id'      => 'require',
-			'tracking_no'     => 'require',
-			'deliver_name'    => 'require',
-			'deliver_phone'   => 'require',
-			'deliver_address' => 'require',
-			'need_express'    => 'require',
-		];
-	protected $message
-		= [
-			'id.require'          => "id必须",
-			'shipper_id.require'  => "商家物流地址必须",
-			'express_id.require'  => "物流公司ID必须",
-			'tracking_no.require' => "物流单号必须",
-		];
-	protected $scene
-		= [
-			'info' => [
-				'id',
-			],
+    protected $rule
+        = [
+            'id'                 => 'require',
+            'shipper_id'         => 'require',
+            'express_id'         => 'require',
+            'tracking_no'        => 'require',
+            'deliver_name'       => 'require',
+            'deliver_phone'      => 'require',
+            'deliver_address'    => 'require',
+            'need_express'       => 'require',
+            'revise_goods'       => 'require|checkReviseGoods',
+            'revise_freight_fee' => 'require|checkReviseFreightFee',
 
-			'setSend' => [
-				'id',
-				'deliver_name',
-				'deliver_phone',
-				'deliver_address',
-				'need_express',
-			],
-
-			'logisticsQuery' => [
-				'express_id',
-				'tracking_no',
-
-			],
-            'groupInfo' => [
+        ];
+    protected $message
+        = [
+            'id.require'                 => "id必须",
+            'shipper_id.require'         => "商家物流地址必须",
+            'express_id.require'         => "物流公司ID必须",
+            'tracking_no.require'        => "物流单号必须",
+            'revise_goods.require'       => "订单商品必须",
+            'revise_freight_fee.require' => "运费必须",
+        ];
+    protected $scene
+        = [
+            'info' => [
                 'id',
             ],
 
-		];
+            'setSend' => [
+                'id',
+                'deliver_name',
+                'deliver_phone',
+                'deliver_address',
+                'need_express',
+            ],
+
+            'logisticsQuery' => [
+                'express_id',
+                'tracking_no',
+
+            ],
+            'groupInfo'      => [
+                'id',
+            ],
+            'changePrice'      => [
+                'revise_goods',
+                'revise_freight_fee',
+            ],
+        ];
+
+    /**
+     * @access protected
+     * @param mixed $value 字段值
+     * @param mixed $rule 验证规则
+     * @return bool
+     */
+    protected function checkReviseGoods($value, $rule, $data)
+    {
+        if (is_array($value)) {
+            foreach ($value as $k => $v) {
+                if (!array_key_exists('id', $v) || !isset($v['id']) || intval($v['id']) <= 0) {
+                    return '参数错误';
+                }
+                if (!array_key_exists('difference_price', $v)) {
+                    return '参数错误';
+                }
+            }
+            $result = true;
+        } else {
+            $result = '参数错误';
+
+        }
+        return $result;
+    }
+
+    /**
+     * @access protected
+     * @param mixed $value 字段值
+     * @param mixed $rule 验证规则
+     * @return bool
+     */
+    protected function checkReviseFreightFee($value, $rule, $data)
+    {
+        if (intval($value) < 0) {
+            $result = '参数错误';
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
 
 }
