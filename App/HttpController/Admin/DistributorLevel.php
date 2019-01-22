36f0a5bdad5756be435249c6ac07186ad8151864
孙泉 <582194766@qq.com>
2019-01-22 18:16:01 +0800

    更新分销代码

---
+++ App/HttpController/Admin/DistributorLevel.php
@@ -0,0 +1,208 @@
+<?php
+/**
+ *
+ * 分销员等级
+ *
+ *
+ *
+ *
+ * @copyright  Copyright (c) 2016-2017 MoJiKeJi Inc. (http://www.fashop.cn)
+ * @license    http://www.fashop.cn
+ * @link       http://www.fashop.cn
+ * @since      File available since Release v1.1
+ */
+
+namespace App\HttpController\Admin;
+
+use App\Utils\Code;
+
+/**
+ * 分销员等级
+ * Class DistributorLevel
+ * @package App\HttpController\Admin
+ */
+class DistributorLevel extends Admin
+{
+
+    /**
+     * 分销员等级
+     * @method GET
+     * @param string title 名称
+     */
+    public function list()
+    {
+        $get           = $this->get;
+        $condition_str = '';
+        $condition     = [];
+        if (isset($get['keywords'])) {
+            $condition['title'] = ['like', '%' . $get['keywords'] . '%'];
+        }
+
+        $distributor_level_model = model('DistributorLevel');
+        $count                   = $distributor_level_model->getDistributorLevelCount($condition, $condition_str);
+        $list                    = $distributor_level_model->getDistributorLevelList($condition, $condition_str, '*', 'id desc', $this->getPageLimit(), '');
+        return $this->send(Code::success, [
+            'total_number' => $count,
+            'list'         => $list,
+        ]);
+    }
+
+    /**
+     * 分销员等级信息
+     * @method GET
+     * @param int id 数据id
+     * @author 孙泉
+     */
+    public function info()
+    {
+        $get   = $this->get;
+        $error = $this->validate($get, 'Admin/DistributorLevel.info');
+        if ($error !== true) {
+            return $this->send(Code::error, [], $error);
+        } else {
+            $distributor_level_model = model('DistributorLevel');
+            $condition               = [];
+            $condition['id']         = $get['id'];
+            $field                   = '*';
+            $info                    = $distributor_level_model->getDistributorLevelInfo($condition, '', $field);
+            return $this->send(Code::success, ['info' => $info]);
+        }
+
+    }
+
+    /**
+     * 分销员等级添加
+     * @method POST
+     * @param int   title               名称
+     * @param int   ratio               佣金比例（第一级）
+     * @param int   invite_ratio        邀请奖励佣金比例（第二级）
+     * @param float promotion_amount    推广金
+     * @param float total_amount        总金额（推广金+消费金）
+     * @param float customer_num        累计客户数
+     * @param float distributor_num     累计分销员数
+     * @param array upgrade_rules       升级规则 ['promotion_amount', 'total_amount', 'customer_num', 'distributor_num'] 至少一种
+     */
+    public function add()
+    {
+        $post  = $this->post;
+        $error = $this->validate($post, 'Admin/DistributorLevel.add');
+        if ($error !== true) {
+            return $this->send(Code::error, [], $error);
+        } else {
+            $distributor_level_model         = model('DistributorLevel');
+            $insert_data                     = [];
+            $insert_data['title']            = $post['title'];
+            $insert_data['ratio']            = floatval($post['ratio']);
+            $insert_data['invite_ratio']     = floatval($post['invite_ratio']);
+            $insert_data['promotion_amount'] = floatval($post['promotion_amount']);
+            $insert_data['total_amount']     = floatval($post['total_amount']);
+            $insert_data['customer_num']     = intval($post['customer_num']);
+            $insert_data['distributor_num']  = intval($post['distributor_num']);
+            $insert_data['upgrade_rules']    = $post['upgrade_rules'];
+            $insert_data['create_time']      = time();
+            $insert_data['update_time']      = time();
+            $result                          = $distributor_level_model->insertDistributorLevel($insert_data);
+
+            if ($result) {
+                return $this->send(Code::success);
+            } else {
+                return $this->send(Code::error);
+            }
+        }
+    }
+
+    /**
+     * 分销员等级编辑
+     * @method POST
+     * @param int   id                  数据id
+     * @param int   title               名称
+     * @param int   ratio               佣金比例（第一级）
+     * @param int   invite_ratio        邀请奖励佣金比例（第二级）
+     * @param float promotion_amount    推广金
+     * @param float total_amount        总金额（推广金+消费金）
+     * @param float customer_num        累计客户数
+     * @param float distributor_num     累计分销员数
+     * @param array upgrade_rules       升级规则 ['promotion_amount', 'total_amount', 'customer_num', 'distributor_num']
+     *                                  至少一种 id为1为空
+     */
+    public function edit()
+    {
+        $post  = $this->post;
+        $error = $this->validate($post, 'Admin/DistributorLevel.edit');
+        if ($error !== true) {
+            return $this->send(Code::error, [], $error);
+        } else {
+            $distributor_level_model = model('DistributorLevel');
+            $condition               = [];
+            $condition['id']         = $post['id'];
+            $level_info              = $distributor_level_model->getDistributorLevelInfo($condition, '', '*');
+            if (!$level_info) {
+                return $this->send(Code::param_error, [], '参数错误');
+            }
+            $update_data                     = [];
+            $update_data['title']            = $post['title'];
+            $update_data['ratio']            = floatval($post['ratio']);
+            $update_data['invite_ratio']     = floatval($post['invite_ratio']);
+            $update_data['promotion_amount'] = floatval($post['promotion_amount']);
+            $update_data['total_amount']     = floatval($post['total_amount']);
+            $update_data['customer_num']     = intval($post['customer_num']);
+            $update_data['distributor_num']  = intval($post['distributor_num']);
+            $update_data['upgrade_rules']    = ($level_info['id'] == 1) ? [] : $post['upgrade_rules'];
+            $update_data['create_time']      = time();
+            $update_data['update_time']      = time();
+            $result                          = $distributor_level_model->updateDistributorLevel(['id' => $level_info['id']], $update_data);
+
+            if ($result) {
+                return $this->send(Code::success);
+            } else {
+                return $this->send(Code::error);
+            }
+        }
+    }
+
+    /**
+     * 删除等级
+     * @method POST
+     * @param int id 数据id
+     * 第一级不可以删除，只能从最高级依次删除。
+     * 分销员等级被删除后，对应的分销员不会被删除，而是会自动依次降级哦。
+     */
+    public function del()
+    {
+        $post  = $this->post;
+        $error = $this->validate($post, 'Admin/DistributorLevel.del');
+        if ($error !== true) {
+            return $this->send(Code::error, [], $error);
+        } else {
+            if ($post['id'] == 1) {
+                return $this->send(Code::param_error, [], '第一级不可以删除');
+            }
+
+            $distributor_level_model = model('DistributorLevel');
+            $condition               = [];
+            $condition['id']         = $post['id'];
+            $distributor_level_info  = $distributor_level_model->getDistributorLevelInfo($condition);
+            if (!$distributor_level_info) {
+                return $this->send(Code::param_error, [], '参数错误');
+            }
+            $max_id = $distributor_level_model->max('id');
+            if ($post['id'] != $max_id) {
+                return $this->send(Code::param_error, [], '只能从最高级依次删除');
+            }
+            $distributor_level_model->startTrans();
+            $distributor_level_result = $distributor_level_model->delDistributorLevel(['id' => $post['id']]);
+            if (!$distributor_level_result) {
+                $distributor_level_model->rollback();
+                return $this->send(Code::error);
+            }
+
+            //TODO 对应的分销员 降级
+
+            $distributor_level_model->commit();
+            return $this->send(Code::success);
+
+        }
+    }
+
+
+}
---
+++ App/Model/DistributionConfig.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * 分销配置
+ *
+ *
+ *
+ *
+ * @copyright  Copyright (c) 2016-2017 MoJiKeJi Inc. (http://www.fashop.cn)
+ * @license    http://www.fashop.cn
+ * @link       http://www.fashop.cn
+ * @since      File available since Release v1.1
+ */
+namespace App\Model;
+
+use ezswoole\Model;
+use traits\model\SoftDelete;
+
+class DistributionConfig extends Model {
+    use SoftDelete;
+    protected $deleteTime    = 'delete_time';
+    protected $resultSetType = 'collection';
+
+    protected $type = [
+        // ''      =>  'json',
+    ];
+
+    /**
+     * 列表
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $field            [字段]
+     * @param  [type] $order            [排序]
+     * @param  [type] $page             [分页]
+     * @param  [type] $group            [分组]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigList($condition = array(), $condition_str = '', $field = '*', $order = 'id desc', $page = '1,20', $group='') {
+        if($page == ''){
+            $data = $this->where($condition)->where($condition_str)->order($order)->field($field)->group($group)->select();
+
+        }else{
+            $data = $this->where($condition)->where($condition_str)->order($order)->field($field)->page($page)->group($group)->select();
+        }
+        return $data ? $data->toArray() : array();
+    }
+
+    /**
+     * 获得数量
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $distinct         [去重]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigCount($condition = array(), $condition_str = '', $distinct = '') {
+        if($distinct == ''){
+            return $this->where($condition)->where($condition_str)->count();
+
+        }else{
+            return $this->where($condition)->where($condition_str)->count("DISTINCT ".$distinct);
+
+        }
+    }
+
+    /**
+     * 列表更多
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $field            [字段]
+     * @param  [type] $order            [排序]
+     * @param  [type] $page             [分页]
+     * @param  [type] $group            [分组]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigMoreList($condition = array(), $condition_str = '', $field = '*', $order = 'id desc', $page = '1,20', $group='') {
+        if($page == ''){
+        $data = $this->alias('xxx1')->join('__XXX2__ xxx2','xxx1.xxx2_id = xxx2.id','LEFT')->where($condition)->where($condition_str)->order($order)->field($field)->group($group)->select();
+
+        }else{
+            $data = $this->alias('xxx1')->join('__XXX2__ xxx2','xxx1.xxx2_id = xxx2.id','LEFT')->where($condition)->where($condition_str)->order($order)->field($field)->page($page)->group($group)->select();
+
+        }
+        return $data ? $data->toArray() : array();
+    }
+
+    /**
+     * 获得数量
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $distinct         [去重]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigMoreCount($condition = array(), $condition_str = '', $distinct = '') {
+        if($distinct == ''){
+            return $this->alias('xxx1')->join('__XXX2__ xxx2','xxx1.xxx2_id = xxx2.id','LEFT')->where($condition)->where($condition_str)->count();
+
+        }else{
+            return $this->alias('xxx1')->join('__XXX2__ xxx2','xxx1.xxx2_id = xxx2.id','LEFT')->where($condition)->where($condition_str)->count("DISTINCT ".$distinct);
+        }
+    }
+
+    /**
+     * 获得信息
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $field            [字段]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigInfo($condition = array(), $condition_str = '', $field = '*') {
+        $data = $this->where($condition)->where($condition_str)->field($field)->find();
+        return $data ? $data->toArray() : array();
+    }
+
+    /**
+     * 获得信息更多
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @param  [type] $field            [字段]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigMoreInfo($condition = array(), $condition_str = '', $field = '*') {
+        $data = $this->alias('xxx1')->join('__XXX2__ xxx2','xxx1.xxx2_id = xxx2.id','LEFT')->where($condition)->where($condition_str)->field($field)->find();
+        return $data ? $data->toArray() : array();
+    }
+
+    /**
+     * 获取的id
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigId($condition = array(), $condition_str = '') {
+        return $this->where($condition)->where($condition_str)->value('id');
+    }
+
+    /**
+     * 获取某个字段
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigValue($condition = array(), $condition_str = '', $field = 'id') {
+        return $this->where($condition)->where($condition_str)->value($field);
+    }
+
+    /**
+     * 获取某个字段列
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function getDistributionConfigColumn($condition = array(), $condition_str = '', $field = 'id') {
+        return $this->where($condition)->where($condition_str)->column($field);
+    }
+
+    /**
+     * 某个字段+1
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function setIncDistributionConfig($condition = array(), $condition_str = '', $field, $num = 1) {
+        return $this->where($condition)->where($condition_str)->setInc($field, $num);
+    }
+
+    /**
+     * 某个字段-1
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function setDecDistributionConfig($condition = array(), $condition_str = '', $field, $num = 1) {
+        return $this->where($condition)->where($condition_str)->setDec($field, $num);
+    }
+
+    /**
+     * 添加单条数据
+     * @param  [type] $insert           [添加数据]
+     */
+    public function insertDistributionConfig($insert = array()) {
+        return $this->save($insert) ? $this->id : false;
+    }
+
+    /**
+     * 添加多条数据
+     * @param  [type] $insert           [添加数据]
+     */
+    public function insertAllDistributionConfig($insert = array()) {
+        return $this->saveAll($insert);
+    }
+
+    /**
+     * 修改信息
+     * @param  [type] $update           [更新数据]
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     * @return [type]                   [数据]
+     */
+    public function updateDistributionConfig($condition = array(),$update = array()) {
+        return $this->save($update,$condition);
+    }
+    /**
+     * 修改多条数据
+     * @param  [type] $update           [更新数据]
+     */
+    public function updateAllDistributionConfig($update = array()) {
+        return $this->saveAll($update);
+    }
+
+    /**
+     * 删除
+     * @param  [type] $condition        [条件]
+     * @param  [type] $condition_str    [条件]
+     */
+    public function delDistributionConfig($condition = array(), $condition_str = '') {
+        return $this->where($condition)->where($condition_str)->delete();
+    }
+
+    /**
+     * 软删除
+     * @param    array  $condition
+     */
+    public function softDelDistributionConfig($condition = array(), $condition_str = '') {
+        return $this->save(['delete_time'=>time()],$condition);
+    }
+
+}
